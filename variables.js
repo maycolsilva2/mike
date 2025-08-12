9511d5b10cd90f6596b9a68fe86ede330bd8e5af
maycolsilva2 <maykolestibensilva@gmail.com>
2025-08-12 12:39:28 -0500

    se agregaron operaciones

--- variables.js
+++ variables.js
@@ -1,4 +1,10 @@
 let numero1= 12;
 let numero2= 8;
 let suma = numero1+numero2;
+let resta = numero1-numero2;
+let multiplicacion = numero1*numero2;
+let division = numero1/numero2;
 console.log(suma);
+console.log(resta);
+console.log(multiplicacion);
+console.log(division);
